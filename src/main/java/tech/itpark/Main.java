b1d5c0b378fb451794981ebb0c51ba9d0532193d
ramil-kham <ramil66-2010@mail.ru>
2021-03-09 20:38:51 +0300

    Initial commit

---
+++ src/main/java/tech/itpark/Main.java
@@ -0,0 +1,44 @@
+package tech.itpark;
+
+import tech.itpark.ad.Film;
+import tech.itpark.domain.Afisha;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Afisha afisha = new Afisha();
+
+        Film film1 = new Film(1,
+                7.1,
+                "https://afisha.yandex.ru/kazan/cinema/para-iz-budushchego?source=selection-events",
+                "Есть билеты",
+                "Пара из будущего",
+                false,
+                "https://afisha.yandex.ru/kazan/cinema/para-iz-budushchego?source=selection-events",
+                7);
+        afisha.add(film1);
+
+        Film film2 = new Film(2,
+                7.8,
+                "https://afisha.yandex.ru/kazan/cinema/raya-i-posledniy-drakon?source=selection-events",
+                "Есть билеты",
+                "Райя и последний дракон",
+                false,
+                "https://afisha.yandex.ru/kazan/cinema/raya-i-posledniy-drakon?source=selection-events",
+                7);
+        afisha.add(film2);
+
+        Film film3 = new Film(3,
+                7.3,
+                "https://afisha.yandex.ru/kazan/cinema/batia-2020?source=selection-events&schedule-preset=today",
+                "Есть билеты",
+                "Батя",
+                false,
+                "https://afisha.yandex.ru/kazan/cinema/batia-2020?source=selection-events&schedule-preset=today",
+                7);
+        afisha.add(film3);
+
+    }
+}
---
+++ src/main/java/tech/itpark/domain/Afisha.java
@@ -0,0 +1,15 @@
+package tech.itpark.domain;
+
+
+import tech.itpark.ad.Film;
+
+import java.util.ArrayList;
+
+public class Afisha {
+    private ArrayList<Film> afisha = new ArrayList<>();
+    public void add (Film film) {
+        afisha.add(film);
+    }
+
+
+}
